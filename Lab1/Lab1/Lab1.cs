c6e02f72ac3fb403feea0a90fd5cb3d3bdadb285
jeongmingi <mo4811@naver.com>
2021-05-03 22:23:04 +0900

    Update Lab1.cs

--- Lab1/Lab1/Lab1.cs
+++ Lab1/Lab1/Lab1.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace Lab1
 {
-    class Lab1
+    public static class Lab1
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
